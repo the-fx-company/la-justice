9ba0f6d6be1da7f45f8f1222bf1faea53e68fc3c
nathanwittersheim <43971842+nathanwittersheim@users.noreply.github.com>
2019-04-03 22:43:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = process.env.token // a garder en version heroku
-const prefix = ("*");
+const token = process.env.token
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
 });
 
-bot.login(token); //a garder en version heroku
+client.on('message', message => {
+  if (!message.guild) return;
+    
+  if (message.content.startsWith('*ban')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.ban({
+          reason: 'They were bad!',
+        }).then(() => {
+          message.reply(`Successfully banned ${user.tag}`);
+        }).catch(err => {
+          message.reply('I was unable to ban the member');
+          console.error(err);
+        });
+      } else {
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+      message.reply('You didn\'t mention the user to ban!');
+    }
+  }
+});
+
+bot.login(token);
