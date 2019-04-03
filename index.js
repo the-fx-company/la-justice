26ba972fce7ec2446bbe6cd14e70bd4187b0b72b
nathanwittersheim <43971842+nathanwittersheim@users.noreply.github.com>
2019-04-03 22:51:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ bot.on('message', message => {
       const member = message.guild.member(user);
       if (member) {
         member.ban({
-          reason: 'They were bad!',
+          reason: 'Parce que c\'est pas gentil d\'être méchant !!!!!!!',
         }).then(() => {
           message.reply(`Successfully banned ${user.tag}`);
         }).catch(err => {
-          message.reply('I was unable to ban the member');
+          message.reply('L\'utilisateur a bien été banni !');
           console.error(err);
         });
       } else {
         message.reply('That user isn\'t in this guild!');
       }
     } else {
-      message.reply('You didn\'t mention the user to ban!');
+      message.reply('Tu dois mentionner la personne que tu veux bannir !');
     }
   }
 });
